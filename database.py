096567ee53d75607e1ea13d7034475d78d4a603f
Mrunal <mrunal1201@gmail.com>
2026-05-12 18:10:15 +0530

    Switch to postgres

--- database.py
+++ database.py
@@ -1,10 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./tasks.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
 
 engine = create_engine(
-	DATABASE_URL, connect_args={"check_same_thread": False}
+	DATABASE_URL, pool_pre_ping=True, connect_args={"check_same_thread": False}
 )
 
 SessionLocal = sessionmaker(bind=engine)
