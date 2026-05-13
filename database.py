c77c86ad58e2b04a8da772fdd4f97eb710a826eb
Mrunal <mrunal1201@gmail.com>
2026-05-13 16:01:12 +0530

    make connect_args conditional to support Render PostgreSQL

--- database.py
+++ database.py
@@ -4,8 +4,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tasks.db")
 
+# Only add check_same_thread if we are using SQLite
+connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+
 engine = create_engine(
-	DATABASE_URL, pool_pre_ping=True, connect_args={"check_same_thread": False}
+	DATABASE_URL, pool_pre_ping=True, connect_args=connect_args
 )
 
 SessionLocal = sessionmaker(bind=engine)
